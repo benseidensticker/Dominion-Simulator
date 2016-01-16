840d80176adc51dc9181c1b912f2300cc87221b4
benseidensticker <benseidensticker@gmail.com>
2016-01-16 12:54:46 -0600

    Initial Updates

--- Java Scripts/src/Card.java
+++
@@ -1,9 +0,0 @@
-/* An object for each individual card in the game. 
- * A card object is not constructed until it's part of a player's deck.
- * Has a cost variable.
- * Has a unique descriptor.
- * 
- */
-public class Card {
-
-}
--- Java Scripts/src/Player.java
+++
@@ -1,8 +0,0 @@
-/* Created for each player in the game.
- * Has a hand, deck, and discard pile assigned.
- * Also victory point amount.
- * 
- */
-public class Player {
-
-}
